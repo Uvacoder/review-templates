2f13d69bd8adffbfaadbfb547cd2f0e541575d7f
markmead <itsmarkmead@gmail.com>
2021-03-25 08:12:35 +0000

    responsive fixes

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,20 +1,20 @@
 <template>
   <div>
     <header class="border-b border-black">
-      <div class="flex items-center h-24 pl-8">
-        <div class="flex-1">
+      <div class="items-center pt-4 space-y-4 sm:flex sm:h-24 sm:pl-8 sm:space-y-0 sm:pt-0">
+        <div class="flex-1 text-center sm:text-left">
           <g-link to="/" class="font-mono text-xl font-bold tracking-widest uppercase"> ⭐️ Marks Reviews </g-link>
         </div>
 
-        <div class="flex space-x-8">
-          <nav class="flex items-center space-x-8">
+        <div class="space-y-4 sm:flex sm:space-x-8 sm:space-y-0">
+          <nav class="flex items-center justify-center space-x-8 sm:justify-end">
             <g-link to="/" class="font-mono font-bold tracking-widest uppercase"> Home </g-link>
             <g-link to="/about/" class="font-mono font-bold tracking-widest uppercase"> About </g-link>
             <g-link to="/reviews/" class="font-mono font-bold tracking-widest uppercase"> Reviews </g-link>
           </nav>
 
           <div class="relative">
-            <div class="inline-flex items-center justify-center h-24 p-8 border-l border-black">
+            <div class="flex items-center justify-center p-8 border-t border-black sm:h-24 sm:border-t-0 sm:border-l">
               <button v-on:click="search = !search" class="font-mono font-bold tracking-widest uppercase">
                 🕵️‍♀️ Search 🕵️
               </button>
@@ -22,7 +22,7 @@
 
             <div
               v-if="search"
-              class="absolute right-0 top-auto z-10 w-screen max-w-3xl p-8 bg-white border border-black"
+              class="absolute right-0 top-auto z-10 w-screen max-w-3xl p-8 bg-white border-t border-b border-black sm:border-l sm:border-r"
             >
               <Search />
             </div>
